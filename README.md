20612de811d88e7f63dc0e7df633f2b0e523a5c2
amconti13 <amconti13@aol.com>
2016-09-26 12:06:38 -0400

    Second version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # findWords
 Reading in files
+Taking a query and finding those words in that file.
+
+This program returns the line numbers that it does or does not exist in.
+
+#findWords2
+A modified version of the first program that has functions.
+- A fuction that uses a default value.
+- Generator function
+*** Go into video and look over end of it for other replacements
--- findWords2.py
+++ findWords2.py
@@ -1,26 +1,68 @@
-filename = input("Enter file to search: ")
-fileobj = open(filename, "r")
 
-worddict = {}
-linenum = 1 #normal people start counting with 1
 
-for line in fileobj :
-    wordlist = line.split()
+#-------------------------------------------------------------------------------
+def get_datalines (filename = "") : #function with default value
+    if not filename : #if they dont give us a file name
+        filename = input("Enter file to earch: ")
+    fileobj = open(filename, "r")
+    data = fileobj.readlines() #read all data lines in one list
+    fileobj.close()
+    return data
 
-    for index in range(0, len(wordlist)) : #for the length of the wordlist
-        wordlist[index] = wordlist[index].strip(",.?!'\"()")
-        wordlist[index] = wordlist[index].upper() #can do either lower or upper
-    #by now list of words are stripped of punctuation and all uppper case
-    for word in wordlist : #adding words into dictionary
-        
-        if word in worddict : #if its in the dictionary already
-            worddict[word].add(linenum) #add line number into the set
-        else : #if its not in the dictionary, add it into the dictionary
-            worddict[word] = {linenum}
+#-------------------------------------------------------------------------------
+def cleanwords(dataline) :
 
-    linenum += 1
+    for word in dataline.split() : #word in list of split up strings from dataline
+        word = word.strip(",.?!'\"()")
+        word = word.upper()
+        yield word #gives it back one by one
 
-fileobj.close() #done processing the file.
+#-------------------------------------------------------------------------------
+def get_worddictionary( lines ) :
+
+    dictionary = {}
+    linenum = 1
+
+    for line in lines :
+        for word in cleanwords(line) : #adding words into dictionary
+            if word in dictionary : #if its in the dictionary already
+                dictionary[word].add(linenum) #add line number into the set
+            else :
+                dictionary[word] = {linenum}
+
+        linenum += 1
+    return dictionary
+#-------------------------------------------------------------------------------
+def replace_word( line, word ) :
+    searchline = line.lower()
+    searchword = word.lower()
+    index = searchline.find(searchword)
+
+    while index != -1 :
+        line = line[ 0 : index ] + word.upper() + line[ index + len(word) : ]
+        index = searchline.find( searchword, index + len(word) )
+    
+    return line
+
+
+
+#-------------------------------------------------------------------------------
+def display_words_in_line( line, linenum, word1, word2 = "" ) :
+    
+    line = replace_word( line, word1 )
+    if word2:
+        line = replace_word( line, word2 )
+    print( linenum, line, sep = ": " )
+
+#-------------------------------------------------------------------------------
+
+
+
+
+datalines = get_datalines("/Users/Conti/Desktop/KKK.txt")
+worddict = get_worddictionary(datalines)
+
+
 
 answer = "yes"
 
@@ -30,12 +72,14 @@ while answer == "yes" :
     
     querylist = query.split() #to be able to look how many items
                               #are in the query list
-        if len(querylist) == 1 : #one item in the query list
-            lines = worddict.get(query.upper())#set of line numbers for the word
+    if len(querylist) == 1 : #one item in the query list
+        lines = worddict.get(query.upper())#set of line numbers for the word
                             # return None or set that is there
         if lines == None : #without this the program will blow up
             print("The word" , query, " does not appear in the file")
         else :
+            for linenum in sorted(list(lines)) :
+                display_words_in_line(datalines[linenum-1], linenum, query)
             print("The word", query, "appears on lines: ", sorted(list(lines)))
 #NOT
     elif len(querylist) == 2 :
